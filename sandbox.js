1e093f3580aa5b11b355cff92a70df9b66f0bdcf
fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-11-18 15:23:22 +0100

    class 1 concluded
    object literals in a nutshell

--- sandbox.js
+++ sandbox.js
@@ -9,3 +9,19 @@ let user = {
 };
 
 console.log(user);
+// to get just one of the properties. using name property
+console.log(user.name);
+// to overwrite one of the property value. Let say age for example
+user.age = 30;
+console.log(user.age);
+// we can also access an update properties using square bracket notation instead of dot notation
+console.log(user['email']);
+user['name'] = 'oluwaniyi';
+console.log(user['name']);
+// one advantage of using aquare bracket is that i can say
+const key = 'location';
+console.log(user[key]);
+// i prefer using dot notation, it pretty easy
+
+console.log(typeof user);
+
