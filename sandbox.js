edd01ebb469430f009439eb56e329b4d5f52d148
fuadoluwaniyi <106689970+fuadoluwaniyi@users.noreply.github.com>
2022-11-18 15:09:36 +0100

    class 1

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+// object literals
+
+let user = {
+    name: 'fuad',
+    age: 22,
+    email: 'fuadoluwaniyi@gmail.com',
+    location: 'lagos',
+    blogs: ['Learning front_end', 'The best time to code is at Night']
+};
+
+console.log(user);
